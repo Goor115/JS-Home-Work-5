10525844b5d92775a5f9fc36a057ca106b026477
Ihor Lapai <Lapayigor@gmail.com>
2022-06-16 14:39:14 +0300

    init 5

---
+++ 2/main.js
@@ -0,0 +1,8 @@
+let a = +prompt(`Вкажыть число А`);
+let b = +prompt(`Вкажыть число B`);
+
+function sum() {
+    sum = a + b ;
+    return sum;
+}
+alert(sum());
---
+++ 3/main.js
@@ -0,0 +1,14 @@
+let a = 2;
+
+function pow() {
+    pow = Math.pow(a, 2);
+    return pow;
+}
+
+function pi() {
+    pi = Math.PI * a;
+    return pi;
+}
+
+alert(pow());
+alert(pi());
